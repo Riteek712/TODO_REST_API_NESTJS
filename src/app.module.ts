3add82f8012fc86f7e2e4610a3058c08ecd91d83
Riteek Rakesh <riteek7.12@gmail.com>
2024-08-10 21:55:11 +0530

    improving cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,19 @@ import { TodoModule } from './todo/todo.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+
 
 @Module({
-  imports: [DatabaseModule, TodoModule, AuthModule],
+  imports: [DatabaseModule, ConfigModule.forRoot(),TodoModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService
+    ,{
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    }
+  ],
 })
 export class AppModule {}
